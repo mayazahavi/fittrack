4c4d95104ce8e9430c52e0224a834e4db0a49b43
mayazahavi <mayazahavi3@gmail.com>
2025-07-19 01:36:53 +0300

    server side- entry model

---
+++ server/models/Entry.js
@@ -0,0 +1,23 @@
+// models/Entry.js
+
+const mongoose = require("mongoose");
+
+const entrySchema = new mongoose.Schema({
+  meals: [
+    {
+      name: String,
+      calories: Number,
+    },
+  ],
+  calories: Number,
+  workout: String,
+  date: String,
+  time: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Entry", entrySchema);
